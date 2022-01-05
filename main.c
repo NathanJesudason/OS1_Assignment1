8e615ceadcc4f3af644778033f4c0d3d3c755bc3
Nathan Jesudason <nathan.jesudason@gmail.com>
2022-01-05 13:21:14 -0800

    added file reading function

--- main.c
+++ main.c
@@ -9,9 +9,35 @@ struct movie {
 	char *languages;
 	double rating_value;
 	struct movie *next;
-
 };
 
+struct movie *createMovie(char *line){
+
+}
+
+struct movie *readFile(char *filePath){
+	FILE *movieFile = fopen(filePath, "r");
+
+	char *Line = NULL;
+	size_t len = 0;
+
+	struct movie *head = NULL;
+	struct movei *tail = NULL;
+
+	while ((nread = getLine(&Line, &len, movieFile)) != -1){
+		struct movie *newNode = createMovie(Line);
+		if (head == NULL){
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail->next = newNode;
+			tail = newNode;
+		}
+	}
+	free(Line);
+	fclose(movieFile);
+	return head;
+}
 
 
 int main(int argc, char * argv[]){
