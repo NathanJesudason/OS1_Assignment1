52771002d11ad2082c72613af7500e54369e71fa
Nathan Jesudason <nathan.jesudason@gmail.com>
2022-01-05 19:09:43 -0800

    fixed errors, created createMovie and start of input handling

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct movie {
 	char *title;
@@ -12,7 +13,25 @@ struct movie {
 };
 
 struct movie *createMovie(char *line){
+	struct movie *currMovie = malloc(sizeof(struct movie));
+	char *saveptr;
+	char *token = strtok_r(line, ",", &saveptr);
+	currMovie->title = calloc(strlen(token) + 1, sizeof(char));
+	strcpy(currMovie->title, token);
 
+	token = strtok_r(NULL, ",", &saveptr);
+	currMovie->year = atoi(token);
+
+	token = strtok_r(NULL, ",", &saveptr);
+	currMovie->languages = calloc(strlen(token) + 1, sizeof(char));
+	strcpy(currMovie->languages, token);
+
+	token = strtok_r(NULL, ",", &saveptr);
+	currMovie->rating_value = strtod(token, NULL);
+
+	currMovie->next= NULL;
+
+	return currMovie;
 }
 
 struct movie *readFile(char *filePath){
@@ -20,11 +39,12 @@ struct movie *readFile(char *filePath){
 
 	char *Line = NULL;
 	size_t len = 0;
+	ssize_t nread;
 
 	struct movie *head = NULL;
-	struct movei *tail = NULL;
+	struct movie *tail = NULL;
 
-	while ((nread = getLine(&Line, &len, movieFile)) != -1){
+	while ((nread = getline(&Line, &len, movieFile)) != -1){
 		struct movie *newNode = createMovie(Line);
 		if (head == NULL){
 			head = newNode;
@@ -39,11 +59,45 @@ struct movie *readFile(char *filePath){
 	return head;
 }
 
+void input_loop(){
+	bool continue_flag = true;
+	while(continue_flag){
+		//print statements
+		//get input
+		char inputbuffer[50];
+		int input = -1;
+		fgets(inputbuffer,50,stdin);
+		input = atoi(inputbuffer);
+		switch(input){
+			case 1:
+				//specify year
+				break;
+			case 2:
+				//highest rated each year
+				break;
+			case 3:
+				//specify language
+				break;
+			case 4:
+				continue_flag = false;
+				break;
+			default:
+				printf("input fail \n");
+		}
+	}
+}
 
 int main(int argc, char * argv[]){
 
 	if(argc < 2){
 		printf("Provide name of file \n");
 		printf("Example: ./movies movie.csv \n");
+		return EXIT_FAILURE;
 	}
+	struct movie *list = readFile(argv[1]);
+	//Processed file [filename] and parsed data for [n] movies
+	input_loop();
+	return EXIT_SUCCESS;
 }
+
+
